a6775a234505359e8e9d1e6d593a47f05b54a9ef
Matthias Koenig <konigmatt@googlemail.com>
2021-06-21 22:19:28 +0200

    itbmeeting

--- meeting.py
+++ meeting.py
@@ -133,6 +133,13 @@ def get_next_dayofweek(d, weekday=1, skip_holidays=True):
                         datetime.date(2020, 12, 22),
                         datetime.date(2020, 12, 29),
                         datetime.date(2021, 1, 5),
+                        datetime.date(2021, 7, 6),
+                        datetime.date(2021, 7, 6),
+                        datetime.date(2021, 7, 13),
+                        datetime.date(2021, 7, 20),
+                        datetime.date(2021, 7, 27),
+                        datetime.date(2021, 8, 3),
+                        datetime.date(2021, 8, 10),
                         ]
         de_holidays = holidays.Germany(years=[2016, 2017])
         while next_d in de_holidays or next_d in itb_holidays:
